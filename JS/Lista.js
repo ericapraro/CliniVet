2e4cc97435e3e584294e6539c03237102c1bcc70
Eric Ruan Capraro <capraro_eric@hotmail.com>
2023-06-19 20:40:57 -0300

    commit

--- JS/Lista.js
+++ JS/Lista.js
@@ -4,9 +4,12 @@ window.addEventListener('load', function () {
   cadPessoa();
 });
 
+// Função que Busca (GET) os dados do BackEnd
 function cadPessoa(nome, tutor, raca) {
 
+  // Função assincrona, pode demorar para obter resposta 
   async function fetchText() {
+    // Fetch Solicita para o back e data transforma em json
     let response = await fetch(`http://localhost:5114/api/Pets`);
     let data = await response.json();
     
@@ -90,30 +93,34 @@ function cadPessoa(nome, tutor, raca) {
   fetchText()
 }
 
+// Função para editar, chama a tela de cadastro passando o Id do cliente por URL para ser tratado por la, recebe o id por parâmetro
 function edit(id){
   window.location = "../Html/cadastro.html?idPet="+id;
   
 }
 
-
+// Função inserir, somente chama a tela de cadastro
 function inserir() {
   window.location.href = '../Html/cadastro.html'
 }
+
 
 async function getLista(){
   let responseDelete = await fetch(`http://localhost:5114/api/Pets`);
   return await Promise.all(responseDelete)
 }
 
-
+// Função assincrona que deleta o cliente
 async function deletar(id) {
+
+  // Fetch faz a solicitação enviando o id 
   const response = await fetch(`http://localhost:5114/api/Pets/${id}`, {
     method: 'DELETE',
     headers: {
       'Content-type': 'application/json'
     },
   });
-
+  // Reload na tela
   location.reload()
   return response;
 }
--- JS/login.js
+++ JS/login.js
@@ -1,6 +1,7 @@
-
+// Função que confere a validação de login
 async function confere() {
 
+    // pega os valores dos campos
     let loginUsuario = document.getElementById('login').value;
     let senhaUsuario = document.getElementById('senha').value;
 
@@ -8,6 +9,7 @@ async function confere() {
       UsuarioLogin: loginUsuario, UsuarioSenha: senhaUsuario
     };
     
+    // fetch faz s solicitação em modo POST para verificar
     await fetch("http://localhost:5114/login", {
       method: "POST",
       headers: {
@@ -18,8 +20,10 @@ async function confere() {
       .then((response) => response.json())
       .then((data) => {
         console.log(data)
+        // caso retorne status 200(OK), ele redireciona
         if(data.status == 200)
           window.location.href = 'clientesLista.html'
+        // Se não dispara um alerta que está errado
         else
           alert('Usuário ou senha incorretos!')
       })
